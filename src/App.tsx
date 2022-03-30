222e6def74e5290a72a3275992554baeae9b3c1b
kmtrom <kylemtrom@gmail.com>
2022-03-29 21:02:15 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * Returns the application
+ * @return {HTMLDivElement} the application
+ */
 function App() {
   return (
     <div className="App">
